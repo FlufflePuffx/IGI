59cfc4ab9b79c75cfab43a1563c62a3db5d93b9f
FlufflePuff <flufflepuffxx@gmail.com>
2023-03-11 21:28:05 +0300

    added calculating process

--- lr1.py
+++ lr1.py
@@ -1,1 +1,26 @@
+def calculate(lhs, rhs, operation):
+    if operation == "add":
+        return lhs + rhs
+
+    elif operation == "sub":
+        return lhs - rhs
+
+    elif operation == "mult":
+        return lhs * rhs
+
+    elif operation == "div":
+        return lhs / rhs
+
 print("Hello World")
+
+lhs_num = input("Введите первое число: ")
+
+rhs_num = input("Введите второе число: ")
+
+operation = input("Введите операцию (add/sub/mult/div): ")
+
+lhs_num = int(lhs_num)
+
+rhs_num = int(rhs_num)
+
+print(calculate(lhs_num, rhs_num, operation))
